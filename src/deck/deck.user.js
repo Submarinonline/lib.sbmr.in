9fdfc2267433a4b8ae59b5c62a40f8756093c5ba
YamaD <yamad@xn--d-8o2b.com>
2021-02-23 18:34:39 +0900

    人間が読めるようになった

--- src/deck/deck.user.js
+++ src/deck/deck.user.js
@@ -1,32 +1,40 @@
 window.cjp = require('cjp')
-;let g=t=>document.querySelector(t)
-    ,a=(d,e,f)=>d.addEventListener(e,f)
-    ,i=()=>{
-        let b=g('.js-compose-text')
-        ;if(!b)return
-        ;let c=g('.js-send-button-container')
-            ,p=c.parentNode
-        ;for(m of p.children){
-            if(m.classList.contains('cjp'))return
+
+const initCjp = () => {
+    const textBox = document.querySelector('.js-compose-text'),
+        tweetButton = document.querySelector('.js-send-button-container')
+
+    if (!textBox) return
+    for (elem of tweetButton.parentNode.children) {
+        if (elem.classList.contains('cjp')) return
+    }
+
+    const cjpButton = document.createElement('button')
+
+    cjpButton.innerText = '怪日语'
+    cjpButton.classList.add('cjp', 'Button--secondary', 'js-show-tip', 'is-disabled')
+    cjpButton.setAttribute('data-original-title', '贵樣！')
+    for (elem of tweetButton.children) {
+        if (elem.classList.contains('btn-extra-height'))
+            cjpButton.classList.add('btn-extra-height', 'padding-v--6', 'padding-h--12')
+    }
+
+    cjpButton.addEventListener('click', () => {
+        textBox.value = cjp.generate(textBox.value)
+        textBox.dispatchEvent(new Event('input'))
+        cjpButton.classList.add('is-disabled')
+    })
+
+    tweetButton.parentNode.insertBefore(cjpButton, tweetButton)
+
+    textBox.addEventListener('input', () => {
+        if (textBox.value) {
+            cjpButton.classList.remove('is-disabled')
+        } else {
+            cjpButton.classList.add('is-disabled')
         }
-        let n=document.createElement('button')
-            ,t=()=>{
-                b.value=cjp.generate(b.value)
-                ;n.classList.add('is-disabled')
-                ;b.dispatchEvent(new Event('input'))
-            }
-            ,s='btn-extra-height'
-            ;n.innerText='怪日语'
-            ;n.setAttribute('data-original-title','贵樣！')
-            ;n.classList.add('cjp','Button--secondary','js-show-tip','is-disabled')
-            ;for(m of c.children){
-                if(m.classList.contains(s))n.classList.add(s,'padding-v--6','padding-h--12')
-            }
-            a(n,'click',t)
-            ;a(b,'input',()=>{
-                if(b.value){n.classList.remove('is-disabled')}else{n.classList.add('is-disabled')}
-            })
-            ;p.insertBefore(n,c)
-        }
-;a(document,'keydown',i)
-;a(document,'pointerdown',i)
+    })
+}
+
+document.addEventListener('keydown', initCjp)
+document.addEventListener('pointerdown', initCjp)
